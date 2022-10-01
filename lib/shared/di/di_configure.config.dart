08fbf7f6522b044887e66685832643164fbab6de
Nguyen Hoang An <an2419998@gmail.com>
2022-10-02 01:29:04 +0700

    WHA-4 resolve bug cannot save new device in the app database

--- lib/shared/di/di_configure.config.dart
+++ lib/shared/di/di_configure.config.dart
@@ -6,13 +6,13 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:hive_flutter/hive_flutter.dart' as _i7;
+import 'package:hive_flutter/hive_flutter.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i6;
+import 'package:shared_preferences/shared_preferences.dart' as _i7;
 
-import '../../data/repositories/device_repository_impl.dart' as _i4;
-import '../../domain/repositories/device_repository.dart' as _i3;
-import '../../domain/usecases/home/home_usecase.dart' as _i5;
+import '../../data/repositories/device_repository_impl.dart' as _i5;
+import '../../domain/repositories/device_repository.dart' as _i4;
+import '../../domain/usecases/home/home_usecase.dart' as _i6;
 import '../../presentation/home/bloc/home_cubit.dart' as _i8;
 import 'module/app_module.dart' as _i9; // ignore_for_file: unnecessary_lambdas
 
@@ -29,21 +29,16 @@ Future<_i1.GetIt> $initGetIt(
     environmentFilter,
   );
   final appModule = _$AppModule();
-  gh.factory<_i3.DeviceRepository>(() => _i4.DeviceRepositoryImpl());
-  gh.factory<_i5.HomeUseCase>(
-      () => _i5.HomeUseCaseImpl(deviceRepository: get<_i3.DeviceRepository>()));
-  await gh.factoryAsync<_i6.SharedPreferences>(
+  gh.factory<_i3.Box<dynamic>>(() => appModule.openDeviceTableBox());
+  gh.factory<_i4.DeviceRepository>(() => _i5.DeviceRepositoryImpl());
+  gh.factory<_i6.HomeUseCase>(
+      () => _i6.HomeUseCaseImpl(deviceRepository: get<_i4.DeviceRepository>()));
+  await gh.factoryAsync<_i7.SharedPreferences>(
     () => appModule.sharedPrefs,
     preResolve: true,
   );
-  gh.factory<String>(
-    () => appModule.deviceTableName,
-    instanceName: 'device_table_name',
-  );
-  gh.factory<_i7.Box<dynamic>>(() => appModule
-      .openDeviceTableBox(get<String>(instanceName: 'device_table_name')));
   gh.factory<_i8.HomeCubit>(
-      () => _i8.HomeCubit(homeUseCase: get<_i5.HomeUseCase>()));
+      () => _i8.HomeCubit(homeUseCase: get<_i6.HomeUseCase>()));
   return get;
 }
 
